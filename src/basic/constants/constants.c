76dc24fe22084d447340c4af5b551e8dfeddf9bc
allecosta <alle_costa10@outlook.com>
2024-12-03 20:56:49 +0100

    feat: added constants

---
+++ src/basic/constants/constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    const int SECONDS_PER_MINUTE = 60;
+    const int MINUTES_PER_HOUR = 60;
+    const float PI = 3.14;
+    printf("Segundos: %d\n", SECONDS_PER_MINUTE);
+    printf("Minutos: %d\n", MINUTES_PER_HOUR);
+    printf("PI: %.2f\n", PI);
+
+    return 0; 
+}
